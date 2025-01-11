aeeb940e13cbf445456ad8d54c0409c6bed564f6
Leila <stambulileila@gmail.com>
2025-01-11 01:01:32 -0500

    update with soup

--- weather_api 2/data/json/weather_api_scrape.py
+++ weather_api 2/data/json/weather_api_scrape.py
@@ -5,9 +5,18 @@ import pandas as pd
 
 #Scrape the open-meteo.com/en/docs site
 URL = "https://api.open-meteo.com/v1/forecast"
+data = {
+    "latitude": -21.72,
+    "longitude": -45.39,
+    "hourly": ["temperature_2m", "relative_humidity_2m", "precipitation", "surface_pressure"],
+    "timezone": "America/New_York",
+    "start_date": "2022-01-01",
+    "end_date": "2023-12-31"
+    
+}
 page = requests.get("https://api.open-meteo.com/v1/forecast")
 soup = BeautifulSoup(page.content, "html.parser")
 
 
-
-#Get the JSON object of the result of page = requests.get("https://api.open-meteo.com/v1/forecast")
+weather_scrape = json.dumps(soup)
+print(weather_scrape)
