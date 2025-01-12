50d365efac19c085fb507c779f13b993e64cf2fa
Leila <stambulileila@gmail.com>
2025-01-11 21:53:03 -0500

    html file now added

--- weather_api 2/data/json/weather_api_scrape.py
+++ weather_api 2/data/json/weather_api_scrape.py
@@ -1,10 +1,11 @@
 import requests
 from bs4 import BeautifulSoup
+import xmltojson
 import json
 import pandas as pd
 
-#Scrape the open-meteo.com/en/docs site
-URL = "https://api.open-meteo.com/v1/forecast"
+#Get the page through get method then Scrape the open-meteo.com/en/docs site
+URL = "https://open-meteo.com/en/docs#hourly=temperature_2m,relative_humidity_2m,precipitation,surface_pressure&timezone=America%2FNew_York&start_date=2022-01-01&end_date=2023-12-31&time_mode=time_interval"
 data = {
     "latitude": -21.72,
     "longitude": -45.39,
@@ -14,9 +15,8 @@ data = {
     "end_date": "2023-12-31"
     
 }
-page = requests.get("https://api.open-meteo.com/v1/forecast")
-soup = BeautifulSoup(page.content, "html.parser")
+page = requests.get("https://open-meteo.com/en/docs#hourly=temperature_2m,relative_humidity_2m,precipitation,surface_pressure&timezone=America%2FNew_York&start_date=2022-01-01&end_date=2023-12-31&time_mode=time_interval")
 
-
-weather_scrape = json.dumps(soup)
-print(weather_scrape)
+#Save Open-Meteo Page Content as weather_scrape.html 
+with open ("weather_scrape.html", "w") as html_file:
+    html_file.write(page.text)
