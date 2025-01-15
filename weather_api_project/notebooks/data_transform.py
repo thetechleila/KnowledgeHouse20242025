8c2cd5a246d4f5fb952524165e44d722baea3a03
Leila <stambulileila@gmail.com>
2025-01-14 23:59:21 -0500

    data tranform update

---
+++ weather_api_project/notebooks/data_transform.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import datetime
+
+#Read the weather_data.csv file
+df = pd.read_csv("weather_api_project/data/csv/weather_data.csv", header=0)
+
+#Remove spaces 
+df.columns = df.columns.str.strip()
+
+#Convert date information to datetime
+
