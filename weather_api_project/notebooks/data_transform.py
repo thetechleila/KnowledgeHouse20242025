6b2cb46eb8b9a405644e133d3b4375ff387fe188
Leila <stambulileila@gmail.com>
2025-01-15 00:05:42 -0500

    touch ups

--- weather_api_project/notebooks/data_transform.py
+++ weather_api_project/notebooks/data_transform.py
@@ -8,4 +8,7 @@ df = pd.read_csv("weather_api_project/data/csv/weather_data.csv", header=0)
 df.columns = df.columns.str.strip()
 
 #Convert date information to datetime
+df.set_index("time")
+df["time"] = pd.to_datetime(df["time"])
+
 
