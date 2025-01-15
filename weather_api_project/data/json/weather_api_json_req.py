52732c1d085a12b77829a3cb8097bade86697b7a
Leila <stambulileila@gmail.com>
2025-01-14 19:57:05 -0500

    finished weather api section with correctly formatted csv

--- weather_api_project/data/json/weather_api_json_req.py
+++ weather_api_project/data/json/weather_api_json_req.py
@@ -5,21 +5,21 @@ import pandas as pd
 #Get the page through get method then Scrape the open-meteo.com/en/docs site
 URL = "https://historical-forecast-api.open-meteo.com/v1/forecast?latitude=-21.72&longitude=-45.39&start_date=2022-01-01&end_date=2023-12-31&hourly=temperature_2m,relative_humidity_2m,precipitation,surface_pressure&timezone=America%2FNew_York"
 
-page = requests.get("https://historical-forecast-api.open-meteo.com/v1/forecast?latitude=-21.72&longitude=-45.39&start_date=2022-01-01&end_date=2023-12-31&hourly=temperature_2m,relative_humidity_2m,precipitation,surface_pressure&timezone=America%2FNew_York")
+page = requests.get(URL)
 
 #Convert page variable into json without metadata
 api_in_json = page.json()
 
-# Nested JSON data
+# Nested JSON data formatted as a dictionary 
+hourly_data = api_in_json ["hourly"] 
 json_data = {
-    "name": ["John", "Jane", "Bob"],
-    "age": [25, 30, 35],
-    "city": ["New York", "San Francisco", "Chicago"],
-    "children": [
-        {"child_name": "Mary", "child_age": 5},
-        {"child_name": "Tom", "child_age": 3},
-        {"child_name": "Kate", "child_age": 2}
-    ]
+    "time": hourly_data["time"],
+    "temperature_2m": hourly_data["temperature_2m"],
+    "relative_humidity_2m": hourly_data["relative_humidity_2m"],
+    "precipitation": hourly_data["precipitation"],
+    "surface_pressure": hourly_data["surface_pressure"]
+    
+
 }
 
 # Convert dictionary to JSON string
