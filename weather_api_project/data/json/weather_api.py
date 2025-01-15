5f700f11ac412c0981b9f4f00266d11020a40f56
Leila <stambulileila@gmail.com>
2025-01-14 22:26:45 -0500

    removed unnecessary files

--- weather_api_project/data/json/weather_api.py
+++ weather_api_project/data/json/weather_api.py
@@ -1,38 +1,31 @@
 import requests
-from bs4 import BeautifulSoup
-import html2text
 import json
 import pandas as pd
 
 #Get the page through get method then Scrape the open-meteo.com/en/docs site
 URL = "https://historical-forecast-api.open-meteo.com/v1/forecast?latitude=-21.72&longitude=-45.39&start_date=2022-01-01&end_date=2023-12-31&hourly=temperature_2m,relative_humidity_2m,precipitation,surface_pressure&timezone=America%2FNew_York"
 
-page = requests.get("https://historical-forecast-api.open-meteo.com/v1/forecast?latitude=-21.72&longitude=-45.39&start_date=2022-01-01&end_date=2023-12-31&hourly=temperature_2m,relative_humidity_2m,precipitation,surface_pressure&timezone=America%2FNew_York")
+page = requests.get(URL)
 
-#Parse Open-Meteo Page and store page in the "soup" variable
-soup = BeautifulSoup(page.content, "html.parser")
+#Convert page variable into json without metadata
+api_in_json = page.json()
 
-#Save Open-Meteo Page Content as weather_scrape.html (just in case & for future reference)
-with open ("weather_scrape.html", "w") as html_file:
-    html_file.write(page.text)
+# Nested JSON data formatted as a dictionary 
+hourly_data = api_in_json ["hourly"] 
+json_data = {
+    "time": hourly_data["time"],
+    "temperature_2m": hourly_data["temperature_2m"],
+    "relative_humidity_2m": hourly_data["relative_humidity_2m"],
+    "precipitation": hourly_data["precipitation"],
+    "surface_pressure": hourly_data["surface_pressure"]
     
-#Remove HTML tag & metadata from the BeautifulSoup variable soup with the get_text() method then saving as json file
 
-#soup.get_text() removes the HTML tags & metadata, leaving only the text
-clean_page = soup.get_text()
+}
 
-#saves cleaned data as weather_json_api.json. The "with" statement automatically closes file 
-with open("weather_json_api.json", "w") as file:
-    file.write(clean_page)
-    
-#Convert the json file 'weather_json_api.json' into a CSV file
-#First step is to convert the json file into a Python dictionary
-with open ("weather_json_api.json", "r") as file:
-    csv_data = file.read()
+# Convert dictionary to JSON string
+json_data = json.dumps(json_data)
 
-parsed_csv_data = json.loads(csv_data)
+# Convert JSON string to Pandas DataFrame
+df = pd.read_json(json_data, orient="columns")
 
-#Second - Create a DataFrame from the dictionary 'parsed_csv_data'
-df = pd.DataFrame.from_dict(parsed_csv_data, orient="columns")
-
-df.to_csv("weather_data.csv", index=False)
+df.to_csv("weather_data.csv", index=False) 
