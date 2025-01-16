be0f16aa5ccb52188578cb77ecbf0c18105cb1b5
Leila <stambulileila@gmail.com>
2025-01-15 20:00:29 -0500

    monthly medians for all available months calculated

--- weather_api_project/notebooks/data_transform.py
+++ weather_api_project/notebooks/data_transform.py
@@ -7,6 +7,9 @@ df = pd.read_csv("weather_api_project/data/csv/weather_data.csv", header=0)
 #Remove spaces 
 df.columns = df.columns.str.strip()
 
-#Convert date information to datetime
-df.set_index("time")
+#Convert date information to datetime 
 df["time"] = pd.to_datetime(df["time"])
+df.set_index("time", inplace=True)
+
+#Calculate monthly medians on temperature, relative humidity, precipitation, and surface pressure then save a new DataFrame
+monthly_medians = df.resample('M').median()
