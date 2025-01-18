cb78d330cf789e0260fcc500569aecec1888d305
SA14 <sa14@sa14s-air.myfiosgateway.com>
2025-01-17 19:04:43 -0500

    filter update

--- weather_api_project/notebooks/data_transform.py
+++ weather_api_project/notebooks/data_transform.py
@@ -18,9 +18,12 @@ monthly_medians = df.resample('M').median()
 yearly_medians = df.resample('Y').median()
 
 #Convert the br_final.csv file into a DataFrame
-df_br_final = pd.read_csv("weather_api_project/data/csv/br_final.csv")
-df_br_final.set_index('year', inplace=True)
+df_br = pd.read_csv("weather_api_project/data/csv/br_final.csv")
+df_br.set_index("year")
+
 
 #Calculate yearly medians on temperature, relative humidity, precipitation, and surface pressure then save a new DataFrame from br_final.csv
 #This dataframe will include the following harvest data: million_60kgs, nonbear_mill_trees, bear_mill_trees, avg_unemp_perc for 2022-2023
-minas_yearly_medians = df.resample('Y', start='2022', end='2023').median()
+minas_df = df_br.filter(items=["subdivision", "non_bear_mills"])
+print(minas_df)
+
