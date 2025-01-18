fac5118ca637368cb3c93e5a991af31004c81c8d
Leila <stambulileila@gmail.com>
2025-01-17 22:45:58 -0500

    monthly and yearly median csv files created

--- weather_api_project/notebooks/data_transform.py
+++ weather_api_project/notebooks/data_transform.py
@@ -4,26 +4,29 @@ import datetime
 #Read the weather_data.csv file
 df = pd.read_csv("weather_api_project/data/csv/weather_data.csv", header=0)
 
-#Remove spaces 
+#Remove spaces and drop null values
 df.columns = df.columns.str.strip().dropna(how="any")
 
-#Convert date information to datetime 
+#Convert date information to datetime then set "time" as the index
 df["time"] = pd.to_datetime(df["time"])
 df.set_index("time", inplace=True)
 
-#Calculate monthly medians on temperature, relative humidity, precipitation, and surface pressure then save a new DataFrame
+#Calculate monthly medians on temperature, relative humidity, precipitation, and surface pressure then save as a new DataFrame
 monthly_medians = df.resample('M').median()
+
+#Convert the monthly medians DataFrame into a CSV file
+monthly_medians.to_csv("monthly_medians_mg.csv")
 
 #Calculate yearly medians on temperature, relative humidity, precipitation, and surface pressure then save a new DataFrame
 yearly_medians = df.resample('Y').median()
 
+#Convert the "yearly_medians" DataFrame into a CSV file
+yearly_medians.to_csv("yearly_medians_mg.csv")
+
 #Convert the br_final.csv file into a DataFrame
-df_br = pd.read_csv("weather_api_project/data/csv/br_final.csv")
-df_br.set_index("year")
+#df_br = pd.read_csv("weather_api_project/data/csv/br_final.csv")
+#df_br.set_index("year")
 
 
 #Calculate yearly medians on temperature, relative humidity, precipitation, and surface pressure then save a new DataFrame from br_final.csv
 #This dataframe will include the following harvest data: million_60kgs, nonbear_mill_trees, bear_mill_trees, avg_unemp_perc for 2022-2023
-minas_df = df_br.filter(items=["subdivision", "non_bear_mills"])
-print(minas_df)
-
