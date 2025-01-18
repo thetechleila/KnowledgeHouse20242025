6cbb571ec6c8d0babb9c8a8b01004095e0f0616c
Leila <stambulileila@gmail.com>
2025-01-17 23:06:48 -0500

    updated column drop

--- weather_api_project/notebooks/data_transform.py
+++ weather_api_project/notebooks/data_transform.py
@@ -11,22 +11,19 @@ df.columns = df.columns.str.strip().dropna(how="any")
 df["time"] = pd.to_datetime(df["time"])
 df.set_index("time", inplace=True)
 
-#Calculate monthly medians on temperature, relative humidity, precipitation, and surface pressure then save as a new DataFrame
+#Calculate monthly medians on temperature, relative humidity, precipitation, and surface pressure for all available months then save as a new DataFrame
 monthly_medians = df.resample('M').median()
 
-#Convert the monthly medians DataFrame into a CSV file
+#Convert the monthly medians DataFrame into a CSV file called "monthly_medians_mg.csv"
 #monthly_medians.to_csv("monthly_medians_mg.csv")
 
-#Calculate yearly medians on temperature, relative humidity, precipitation, and surface pressure then save a new DataFrame
+#Calculate yearly medians on temperature, relative humidity, precipitation, and surface pressure for all available years then save a new DataFrame
 yearly_medians = df.resample('Y').median()
 
-#Convert the "yearly_medians" DataFrame into a CSV file
+#Convert the "yearly_medians" DataFrame into a CSV file called "yearly_medians_mg.csv"
 #yearly_medians.to_csv("yearly_medians_mg.csv")
 
-#Convert the br_final.csv file into a DataFrame and drop null values
+#Convert the br_final.csv file into a DataFrame then drop the "avg_unemp_perc" column to remove null data
 df_br1 = pd.read_csv("weather_api_project/data/csv/br_final.csv")
-df_br1.dropna(inplace=True)
+df_br1.drop(columns="avg_unemp_perc", inplace=True)
 
-
-#Calculate yearly medians on temperature, relative humidity, precipitation, and surface pressure then save a new DataFrame from br_final.csv
-#This dataframe will include the following harvest data: million_60kgs, nonbear_mill_trees, bear_mill_trees, avg_unemp_perc for 2022-2023
